50453c6db4275e6eba72f472bda68c7ec85e2daf
Ondra907 <ondra907@email.cz>
2023-05-27 20:58:44 +0200

    😼 Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
+# Cats detection using OpenCV library.
+# https://github.com/Ondra9071/catsdetector/
+
 import cv2
 
-video = 'assets/vids/cats.mp4'
-# def detect_cats(video_path):
-cat_cascade = cv2.CascadeClassifier('assets/xml/cats.xml')
+def detect_cats(video_path):
+    xml = cv2.CascadeClassifier('assets/xml/cats.xml')
 
-video = cv2.VideoCapture(video)
-print("""Simple cats face detector using these assets:
-cv2 library,
-XML file from OpenCV GitHub: https://github.com/opencv/opencv/tree/master/data/haarcascades,
-and this YouTube video: https://www.youtube.com/watch?v=-5CdAup0o-I
+    source = cv2.VideoCapture(video_path)
 
-https://www.linkedin.com/in/ondrejpacovsky/
-https://github.com/Ondra9071""")
+    while True:
+        ret, frame = source.read()
+        if not ret:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cats = xml.detectMultiScale(gray, 1.1, 3)
 
-while True:
-    ret, frame = video.read()
+        for (x, y, w, h) in cats:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    if not ret:
-        break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Detekce koček', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    cats = cat_cascade.detectMultiScale(gray, 1.1, 3)
+    source.release()
+    cv2.destroyAllWindows()
 
-    for (x, y, w, h) in cats:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.imshow('linkedin.com/in/ondrejpacovsky', frame)
-
-video.release()
-cv2.destroyAllWindows()
+video_path = 'assets/vids/cats.mp4'
+detect_cats(video_path)
